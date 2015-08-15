3c719c47180b3fef56e9ac5ff1cceed868b8d746
Victor Pereira <vpereira@suse.de>
2015-08-16 00:18:40 +0200

    refactoring

--- lib/cvssv2.rb
+++ lib/cvssv2.rb
@@ -106,12 +106,36 @@ module Cvssv2
       @cdp,@td,@cr,@ir,@ar = @vector.scan(VECTOR_REGEXP).flatten
     end
 
+    def access_complexity
+      AccessComplexity.score(@ac)
+    end
+
+    def authentication
+      Authentication.score(@au)
+    end
+
+    def confidentiality
+      AccessVector.score(@av)
+    end
+
+    def confidentiality_impact
+      ConfidentialityImpact.score(@c)
+    end
+
+    def integrity_impact
+      IntegrityImpact.score(@i)
+    end
+
+    def availability_impact
+      AvailabilityImpact.score(@a)
+    end
+
     def impact
-      sprintf("%.2f",10.41 * (1.0 - (1.0 - ConfidentialityImpact.score(@c)) * (1.0 - IntegrityImpact.score(@i)) * (1.0-AvailabilityImpact.score(@a)))).to_f
+      sprintf("%.2f",10.41 * (1.0 - (1.0 - confidentiality_impact) * (1.0 - integrity_impact) * (1.0- availability_impact))).to_f
     end
 
     def exploitability
-      sprintf("%.2f",20 * AccessComplexity.score(@ac) * Authentication.score(@au) * AccessVector.score(@av)).to_f
+      sprintf("%.2f",20 * access_complexity * authentication * confidentiality).to_f
     end
 
     def base_score
